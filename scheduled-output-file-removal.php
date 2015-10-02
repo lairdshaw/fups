d2f71892621dad499638dc788d663b7984c2a0c2
Laird Shaw <lairdshaw77@gmail.com>
2015-10-03 04:40:59 +1000

    Bugfix: scheduled file deletion wasn't deleting image dirs.

--- scheduled-output-file-removal.php
+++ scheduled-output-file-removal.php
@@ -4,29 +4,49 @@ require_once __DIR__.'/common.php';
 
 $prefix = 'fups.output.';
 $min_delete_age = FUPS_SCHEDULED_DELETION_MIN_AGE_IN_DAYS * 24 * 60 * 60; // in seconds
-$excluded_dirs = array('.', '..');
 
-if ($dh = opendir(FUPS_OUTPUTDIR)) {
-	while (($file = readdir($dh)) !== false) {
-		if (is_dir(FUPS_OUTPUTDIR.$file) && !in_array($file, $excluded_dirs) && time() - stat(FUPS_OUTPUTDIR.$file)['mtime'] > $min_delete_age) {
-			delete_dir_older_than_files(FUPS_OUTPUTDIR.$file, $min_delete_age);
-			rmdir(FUPS_OUTPUTDIR.$file);
-		}
-	}
-	closedir($dh);
-} else	fwrite(STDERR, 'Error: failed to open directory "'.FUPS_OUTPUTDIR."\".\n");
+delete_files_in_dir_older_than_r(FUPS_DATADIR, $min_delete_age, false, array('.htaccess'));
 
-delete_dir_older_than_files(FUPS_DATADIR, $min_delete_age);
+/* $excluded_files applies to the top level only */
+function delete_files_in_dir_older_than_r($dir, $min_delete_age, $delete_dir_too = false, $excluded_files = array()) {
+	static $excluded_dirs = array('.', '..');
 
-function delete_dir_older_than_files($dir, $min_delete_age) {
+	$dir_is_empty = true;
+	// Stat before making changes.
+	$dir_m_time = stat($dir)['mtime'];
+
 	if ($dh = opendir($dir)) {
 		while (($file = readdir($dh)) !== false) {
-			if (is_file($dir.'/'.$file) && time() - stat($dir.'/'.$file)['mtime'] > $min_delete_age) {
-				unlink($dir.'/'.$file);
-			}	
+			// Ignore . and ..
+			if (in_array($file, $excluded_dirs)) continue;
+
+			if (in_array($file, $excluded_files)) {
+				$dir_is_empty = false;
+				continue;
+			}
+			$filepath = $dir.'/'.$file;
+			if (is_file($filepath)) {
+				if (time() - stat($filepath)['mtime'] > $min_delete_age) {
+					if (!unlink($filepath)) {
+						fwrite(STDERR, 'Non-fatal error: failed to unlink file "'.$filepath."\"\n");
+						$dir_is_empty = false;
+					}
+				} else	$dir_is_empty = false;
+			} else if (is_dir($filepath) && !delete_files_in_dir_older_than_r($filepath, $min_delete_age, true)) {
+				$dir_is_empty = false;
+			}
 		}
 	} else	fwrite(STDERR, 'Non-fatal error: failed to open directory "'.$dir."\".\n");
 	closedir($dh);
+
+	if ($delete_dir_too && $dir_is_empty && time() - $dir_m_time > $min_delete_age) {
+		if (!rmdir($dir)) {
+			fwrite(STDERR, 'Non-fatal error: failed to remove directory "'.$dir."\"\n");
+			$dir_is_empty = false;
+		}
+	}
+
+	return $dir_is_empty;
 }
 
 ?>
